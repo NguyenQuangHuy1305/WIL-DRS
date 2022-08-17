a9a811a404dc2b5961e9471d6a4f22aab4681c84
NguyenQuangHuy1305 <mr831817toe@gmail.com>
2022-08-18 00:20:53 +1000

    watching video #8

--- webpage.py
+++ webpage.py
@@ -1,18 +1,79 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "test_secret_key"
+# config for the table users
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+# do not track modifications to the db (maybe to get rid of notifications)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", name=name)
+app.permanent_session_lifetime = timedelta(days=5)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+# @app.route("/<name>")
+# def home(name):
+#     return render_template("index.html", name=name)
 
 # @app.route("/<name>")
 # def user(name):
 #     return f"Hello {name}!"
 
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="you're not admin!"))
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash("Login successful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in!")
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash("Email was saved!")
+        else:
+            if "email" in session:
+                email = session["email"]
+
+        return render_template("user.html", email=email)
+    else:
+        flash("You are not logged in!")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    flash(f"You have been logged out", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name="you're not admin!"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
